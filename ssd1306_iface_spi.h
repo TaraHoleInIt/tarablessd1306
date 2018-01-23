ae0e095573cce3d84e90030a9422026eea9039c6
TaraHoleInIt <tara.a.k.86@gmail.com>
2018-01-22 22:56:16 -0500

    *slaps forehead*

--- ssd1306_iface_spi.h
+++ ssd1306_iface_spi.h
@@ -6,6 +6,6 @@ int ESP32_WriteData_SPI( struct SSD1306_Device* DeviceHandle, uint8_t* Data, siz
 void ESP32_SPI_ResetDisplay( void);
 int ESP32_InitSPIMaster( void );
 int ESP32_AddDevice_SPI( struct SSD1306_Device* DeviceHandle, int Width, int Height, int CSPin, int RSTPin );
-void ESP32_SetSPIConfig( spi_host_device_t SPIHost, int MOSI, int MISO, int SCK, int DC, int ClockSpeedHz );
+void ESP32_SetConfig_SPI( spi_host_device_t SPIHost, int MOSI, int MISO, int SCK, int DC, int ClockSpeedHz );
 
 #endif
